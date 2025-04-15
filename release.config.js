2b88b82c3b1389ec9813145f31b76cfa115b2f1d
Chris Leonavicius <chris.leonavicius@amplitude.com>
2025-04-15 10:55:31 -0700

    fix: add podspec

--- release.config.js
+++ release.config.js
@@ -18,54 +18,54 @@ module.exports = {
         "assets": [
           { "path": "AmplitudeCore.zip" },
         ]
-    }],
-     [
-       "@google/semantic-release-replace-plugin",
-       {
-         "replacements": [
-    //       {
-    //         "files": ["AmplitudeCore.podspec"],
-    //         "from": "amplitude_core_version = \".*\"",
-    //         "to": "amplitude_core_version = \"${nextRelease.version}\"",
-    //         "results": [
-    //           {
-    //             "file": "AmplitudeCore.podspec",
-    //             "hasChanged": true,
-    //             "numMatches": 1,
-    //             "numReplacements": 1
-    //           }
-    //         ],
-    //         "countMatches": true
-    //       },
-          {
-            "files": ["Package.swift", "Package@swift-5.9.swift"],
-            "from": "https://github.com/amplitude/AmplitudeCore-Swift/releases/download/v.*/AmplitudeCore.zip",
-            "to": "https://github.com/amplitude/AmplitudeCore-Swift/releases/download/v${nextRelease.version}/AmplitudeCore.zip",
-            "results": [
-              {
-                "file": "Package.swift",
-                "hasChanged": true,
-                "numMatches": 1,
-                "numReplacements": 1
-              },
-              {
-                "file": "Package@swift-5.9.swift",
-                "hasChanged": true,
-                "numMatches": 1,
-                "numReplacements": 1
-              }
-            ],
-            "countMatches": true
-          },
-        ]
-      }
+      }],
+      [
+        "@google/semantic-release-replace-plugin",
+        {
+          "replacements": [
+            {
+              "files": ["AmplitudeCore.podspec"],
+              "from": "amplitude_core_version = \".*\"",
+              "to": "amplitude_core_version = \"${nextRelease.version}\"",
+              "results": [
+                {
+                  "file": "AmplitudeCore.podspec",
+                  "hasChanged": true,
+                  "numMatches": 1,
+                  "numReplacements": 1
+                }
+              ],
+              "countMatches": true
+            },
+            {
+              "files": ["Package.swift", "Package@swift-5.9.swift"],
+              "from": "https://github.com/amplitude/AmplitudeCore-Swift/releases/download/v.*/AmplitudeCore.zip",
+              "to": "https://github.com/amplitude/AmplitudeCore-Swift/releases/download/v${nextRelease.version}/AmplitudeCore.zip",
+              "results": [
+                {
+                  "file": "Package.swift",
+                  "hasChanged": true,
+                  "numMatches": 1,
+                  "numReplacements": 1
+                },
+                {
+                  "file": "Package@swift-5.9.swift",
+                  "hasChanged": true,
+                  "numMatches": 1,
+                  "numReplacements": 1
+                }
+              ],
+              "countMatches": true
+            },
+          ]
+        }
+      ],
+      ["@semantic-release/git", {
+        "assets": ["AmplitudeCore.podspec", "CHANGELOG.md", "Package.swift", "Package@swift-5.9.swift"],
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }],
+       ["@semantic-release/exec", {
+         "publishCmd": "pod trunk push AmplitudeCore.podspec",
+       }],
     ],
-    ["@semantic-release/git", {
-      "assets": [/*"AmplitudeCore.podspec", */"CHANGELOG.md", "Package.swift", "Package@swift-5.9.swift"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    // ["@semantic-release/exec", {
-    //   "publishCmd": "pod trunk push AmplitudeCore.podspec --allow-warnings",
-    // }],
-  ],
-}
+  }
