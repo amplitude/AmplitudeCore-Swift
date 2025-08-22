313a8e985a9939d4cb2378259027d60b96781855
Jin Xu <jin.xu@amplitude.com>
2025-08-22 11:13:36 -0700

    fix: avoid supported version deprecated warning (#41)
    * fix: avoid supported version deprecated warning
    
    * fix relase script

---
+++ Package@swift-6.2.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "AmplitudeCore",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v4),
+        .visionOS(.v1),
+    ],
+    products: [
+        .library(
+            name: "AmplitudeCore",
+            targets: ["AmplitudeCore"]),
+        .library(
+            name: "AmplitudeCoreFramework",
+            targets: ["AmplitudeCoreFramework"])
+    ],
+    targets: [
+        .target(
+            name: "AmplitudeCore",
+            resources: [.process("PrivacyInfo.xcprivacy")]),
+        .binaryTarget(
+            name: "AmplitudeCoreFramework",
+            url: "https://github.com/amplitude/AmplitudeCore-Swift/releases/download/v1.2.1/AmplitudeCore.zip",
+            checksum: "3f4c482bccc527211e31e33d51d05b4953af071a1fddc5a706e52dcabbdb0446"),
+        .testTarget(
+            name: "AmplitudeCoreTests",
+            dependencies: ["AmplitudeCore"])
+    ]
+)
